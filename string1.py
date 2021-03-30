39de91e102bc7abefe6e720f0f11baa4eefc69ab
Uri-H <55294035+Uri-H@users.noreply.github.com>
2021-03-30 17:24:26 -0400

    Add files via upload
    A couple of python files I wrote while learning python from a google course

---
+++ string1.py
@@ -0,0 +1,75 @@
+def donuts(count):
+   if count < 10:
+      a = 'Number of donuts: ' + str(count)
+   else:
+      a = 'Number of donuts: many'
+   return a
+
+
+def both_ends(s):
+    if len(s) < 2:
+        return ''
+    else:
+        a = s[:2] + s[-2:]
+    return a
+
+
+
+def fix_start(s):
+    c = s[0]
+    a = s.replace(s[0],'*')
+    d = c + a[1:]
+  # +++your code here+++
+    return d
+
+def mix_up(a, b):
+    c = b[:2] + a[2:] + ' ' + a[:2] + b[2:]
+  # +++your code here+++
+    return c
+
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+
+# Provided main() calls the above functions with interesting inputs,
+# using test() to check if each result is correct or not.
+def main():
+  print 'donuts'
+  # Each line calls donuts, compares its result to the expected for that call.
+  test(donuts(4), 'Number of donuts: 4')
+  test(donuts(9), 'Number of donuts: 9')
+  test(donuts(10), 'Number of donuts: many')
+  test(donuts(99), 'Number of donuts: many')
+
+  print
+  print 'both_ends'
+  test(both_ends('spring'), 'spng')
+  test(both_ends('Hello'), 'Helo')
+  test(both_ends('a'), '')
+  test(both_ends('xyz'), 'xyyz')
+
+  
+  print
+  print 'fix_start'
+  test(fix_start('babble'), 'ba**le')
+  test(fix_start('aardvark'), 'a*rdv*rk')
+  test(fix_start('google'), 'goo*le')
+  test(fix_start('donut'), 'donut')
+
+  print
+  print 'mix_up'
+  test(mix_up('mix', 'pod'), 'pox mid')
+  test(mix_up('dog', 'dinner'), 'dig donner')
+  test(mix_up('gnash', 'sport'), 'spash gnort')
+  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+  main()
+
